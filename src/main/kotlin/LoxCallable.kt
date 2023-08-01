6faf4095c8a262ffe5211737cf239f33b63a3b4f
Anna Melnikov <anna.me@hey.com>
2023-08-01 14:44:57 -0500

    fix: fix return type on call method for LoxCallable

--- src/main/kotlin/LoxCallable.kt
+++ src/main/kotlin/LoxCallable.kt
@@ -2,5 +2,5 @@ package com.craftinginterpreters.lox
 
 interface LoxCallable {
     val arity: Int
-    fun call(interpreter: Interpreter, arguments: List<Any?>)
+    fun call(interpreter: Interpreter, arguments: List<Any?>): Any?
 }
